40705308f23ac3929df824882229d893f0a0bbe9
Đỗ Anh Vũ <71638421+vudomc1012@users.noreply.github.com>
2022-11-21 13:24:49 +0700

    Update latest
    card - border

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'A little gift for LTH';
+const giftUrl = 'https://youtu.be/vGJTaP6anOU';
+const eventName = 'Merry Chít mớt hơ nìiii ';
+const titleCard = 'Tặng Lê Thanh Hiền';
+const contentCard = 'Merry Christmas Make A Wish. Dear my loyal princess, I hope you receive everything you wish for this year. You deserve the best! Thanks for being such agreat girl friend.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Anh Vu Do https://www.facebook.com/chedauxanhne/
+
+const giftImage = 'LTHxinkdep.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
