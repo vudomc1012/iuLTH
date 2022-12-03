b884e16b54d20b283c3f3f91376185ca4f83bb96
MSI\vudom <vudaph15914@fpt.edu.vn>
2022-12-03 16:19:59 +0700

    update gift up

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ const titleCard = 'Tặng Lê Thanh Hiền';
 const contentCard = 'Merry Christmas Make A Wish. Dear my loyal princess, I hope you receive everything you wish for this year. You deserve the best! Thanks for being such agreat girl friend.💘';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
 // Anh Vu Do https://www.facebook.com/chedauxanhne/
 
 const giftImage = 'LTHxinkdep.jpg';
