dae42e16242a69c6a5cc3b561cf8ec8c73f41f7c
MSI\vudom <vudaph15914@fpt.edu.vn>
2022-12-15 17:49:51 +0700

    update snow

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'A little gift for LTH';
-const giftUrl = 'https://youtu.be/vGJTaP6anOU';
+const giftUrl = 'https://youtu.be/54aAADpMkTA';
 const eventName = 'Merry chít mớt hơ nìiii ';
 const titleCard = 'Dear Le Thanh Hien';
 const contentCard = 'Darling, you are so beautiful in my eyes. I like your smile very much. I wonder if you′re smiling when you are seeing this gift. Merry Christmas and wish all your wishes come true. Thank you for showing up in my life.';
