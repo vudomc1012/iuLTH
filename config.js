6d0961fefcd29a46ceb71219b65ea2eaa5c6c75d
MSI\vudom <vudaph15914@fpt.edu.vn>
2022-12-15 17:28:59 +0700

    update snow

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const nameGirl = 'A little gift for LTH';
 const giftUrl = 'https://youtu.be/vGJTaP6anOU';
 const eventName = 'Merry chít mớt hơ nìiii ';
 const titleCard = 'Dear Le Thanh Hien';
-const contentCard = 'Darling, you are so beautiful in my eyes. I like your smile very much. I wonder if you’re smiling when you are seeing this gift. Merry Christmas and wish all your wishes come true. Thank you for showing up in my life.';
+const contentCard = 'Darling, you are so beautiful in my eyes. I like your smile very much. I wonder if you′re smiling when you are seeing this gift. Merry Christmas and wish all your wishes come true. Thank you for showing up in my life.';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// phần dưới dành code, nếu muốn chỉnh ảnh đơn giản với base64
 // Anh Vu Do https://www.facebook.com/chedauxanhne/
 
 const giftImage = 'LTHismine.jpg';
